cf2ae4bbc689ecf080d395aea7805ca69f5b7f75
ka2nq1 <ka2nq1@gmail.com>
2021-01-19 19:25:17 +0200

    pages

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+
+export default class App extends Component {
+    state = {
+    };
+
+    render() {
+        return (
+            <div className={styles.container}></div>
+        )
+    }
+};
